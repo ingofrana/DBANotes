95c59437fad63bff075b5da42ec9cbdc4168838c
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-09 01:38:27 -0300

    Cleaning project from notes-game

--- front/src/pages/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export default class extends React.Component {
-    render() {
-        return (
-            <div>PÁGINA NÃO ENCONTRADA!</div>
-        )
-    }
-}
